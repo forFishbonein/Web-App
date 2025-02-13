5dde38cfaa5211db68c50d134754bfb194781e41
DeshpandeVaishnavi5 <vdeshpande@sotersafetyconsulting.com>
2025-02-13 01:19:32 +0000

    Frontend structure along with sign-up, login and verification

---
+++ fitness-frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import AuthenticationPage from "./pages/authenticationPage";
+
+function App() {
+  const isAuthenticated = false; // Replace with actual authentication logic
+
+  return (
+    <Router>
+      {isAuthenticated ? (
+        <>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            {/* Catch all unhandled routes */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          <Route path="/authenticate" element={<AuthenticationPage />} />
+          {/* Redirect all non-authenticated users to login */}
+          <Route path="*" element={<Navigate to="/authenticate" />} />
+        </Routes>
+      )}
+    </Router>
+  );
+}
+
+export default App;
---
+++ fitness-frontend/src/components/login/loginForm.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./LoginForm.css";
+import {
+  TextField,
+  Button,
+  Container,
+  Box,
+  Link,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.email = formData.email ? "" : "Email is required";
+    tempErrors.password = formData.password ? "" : "Password is required";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every((x) => x === "");
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validate()) {
+      try {
+        const response = await axios.post("http://localhost:8060/auth/login", {
+          email: formData.email,
+          password: formData.password,
+        });
+        if (response.status === 200) {
+          setMessage("Login Successful!");
+          navigate("/home"); // Redirect to home
+        }
+      } catch (error) {
+        setMessage(
+          error.response?.data?.message || "Login failed. Please try again."
+        );
+      }
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            error={!!errors.email}
+            helperText={errors.email}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            error={!!errors.password}
+            helperText={errors.password}
+            margin="normal"
+            slotProps={{
+              input: {
+                endAdornment: formData.password ? (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ) : null,
+              },
+            }}
+          />
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt={1}
+          >
+            <Link
+              href="#"
+              variant="body2"
+              sx={{ color: "#224a95", textDecorationColor: "#224a95" }}
+            >
+              Forgot password?
+            </Link>
+          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            className="login-btn"
+          >
+            Login
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginForm;
